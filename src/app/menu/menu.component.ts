1f1779e457add3117e54bfb80447baaf6e45d87f
Viktor Sivulskiy <svicvg@gmail.com>
2018-03-28 13:20:56 +0600

    Data Binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,38 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Dish } from '../shared/dish';
-
-const DISHES: Dish[] = [
-  {
-    name:'Чашушули',
-    image: '/assets/images/chashushuli.jpg',
-    category: 'Основные',
-    label:'Hot',
-    price:'1 699',
-    description:'Блюдо из мяса говядины (телятины). В переводе с грузинского обозначает - острый, так как это блюдо приготовлено с большим количеством острого перца, в собственном соку, что обеспечивает ему неповторимый вкус, очень мягкий и острый одновременно.'                        },
- {
-    name:'Пхали',
-    image: '/assets/images/phali.jpg',
-    category: 'Закуски',
-    label:'',
-    price:'1 699',
-    description:'Грузинское блюдо Пхали - это холодная закуска, которая может делаться из различных овощей и трав, но неизменным ее ингредиентом являются орехи и специи. Наш вкусный Пхали запомнится Вам надолго.'                        },
- {
-    name:'Чахохбили',
-    image: '/assets/images/chahohbili.jpg',
-    category: 'Основные',
-    label:'New',
-    price:'1 699',
-    description:'Блюдо дня - аппетитный чахохбили! Первоначально делалось из фазана (груз. ხოხობი — [хохоби]), но в настоящее время из любого мяса птицы, а особенно домашней курицы.'                        },
- {
-    name:'Пахлава',
-    image: '/assets/images/pahlava.jpg',
-    category: 'Десерт',
-    label:'',
-    price:'699',
-    description:'И в Завершении трапезы не забудьте вкусить сладость сочнейшей свежей "Пахлавы". Это популярное кондитерское изделие из слоёного теста с орехами в сиропе.'                        }
- ];
-
+import { DISHES } from '../shared/dishes';
 
 @Component({
   selector: 'app-menu',
@@ -43,11 +11,15 @@ export class MenuComponent implements OnInit {
 
   dishes = DISHES;
 
-  selectedDish = DISHES[0];
-  
+  selectedDish: Dish;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(dish: Dish) {
+    this.selectedDish = dish;
   }
 
 }
---
+++ src/app/shared/dishes.ts
@@ -0,0 +1,164 @@
+import { Dish } from '../shared/dish';
+
+export const DISHES: Dish[] = [
+  {
+    name: 'Чашушули',
+    image: '/assets/images/chashushuli.jpg',
+    category: 'Основные',
+    label: 'Hot',
+    price: '1 699',
+    description: 'Блюдо из мяса говядины (телятины). В переводе с грузинского обозначает - острый, так как это блюдо приготовлено с большим количеством острого перца, в собственном соку, что обеспечивает ему неповторимый вкус, очень мягкий и острый одновременно.',
+    comments: [
+      {
+        rating: 5,
+        comment: "Imagine all the eatables, living in Dadiani!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+      },
+      {
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    name: 'Пхали',
+    image: '/assets/images/phali.jpg',
+    category: 'Закуски',
+    label: '',
+    price: '1 699',
+    description: 'Грузинское блюдо Пхали - это холодная закуска, которая может делаться из различных овощей и трав, но неизменным ее ингредиентом являются орехи и специи. Наш вкусный Пхали запомнится Вам надолго.',
+    comments: [
+      {
+        rating: 5,
+        comment: "Imagine all the eatables, living in Dadiani!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+      },
+      {
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    name: 'Чахохбили',
+    image: '/assets/images/chahohbili.jpg',
+    category: 'Основные',
+    label: 'New',
+    price: '1 699',
+    description: 'Блюдо дня - аппетитный чахохбили! Первоначально делалось из фазана (груз. ხოხობი — [хохоби]), но в настоящее время из любого мяса птицы, а особенно домашней курицы.',
+    comments: [
+      {
+        rating: 5,
+        comment: "Imagine all the eatables, living in Dadiani!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+      },
+      {
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+      }
+    ]
+  },
+  {
+    name: 'Пахлава',
+    image: '/assets/images/pahlava.jpg',
+    category: 'Десерт',
+    label: '',
+    price: '699',
+    description: 'И в Завершении трапезы не забудьте вкусить сладость сочнейшей свежей "Пахлавы". Это популярное кондитерское изделие из слоёного теста с орехами в сиропе.',
+    comments: [
+      {
+        rating: 5,
+        comment: "Imagine all the eatables, living in Dadiani!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+      },
+      {
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+      },
+      {
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+      },
+      {
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+      }
+    ]
+  }
+];
