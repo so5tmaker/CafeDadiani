e16fcc74dcd6b8651b9d94a78101fb933577b89e
Viktor Sivulskiy <svicvg@gmail.com>
2018-04-05 13:18:48 +0600

    The Const PROMOTIONS Was Added

---
+++ src/app/shared/promotions.ts
@@ -0,0 +1,13 @@
+import { Promotion } from './promotion';
+
+export const PROMOTIONS: Promotion[] = [
+    {
+      id: 0,
+      name: 'Чашушули',
+      image: '/assets/images/chashushuli.jpg',
+      label: 'New',
+      price: '1 699',
+      featured: true,
+      description: 'Блюдо из мяса говядины (телятины). В переводе с грузинского обозначает - острый, так как это блюдо приготовлено с большим количеством острого перца, в собственном соку, что обеспечивает ему неповторимый вкус, очень мягкий и острый одновременно.'
+    }
+  ];
