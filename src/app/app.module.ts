edcb998ba52e93eb20fa109249c737b38a54f55c
Viktor Sivulskiy <svicvg@gmail.com>
2018-04-05 13:24:48 +0600

    The Service Promotion Was Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -44,7 +45,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatCardModule, MatIconModule, MatProgressSpinnerModule, MatDialogModule,
     FlexLayoutModule, AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
