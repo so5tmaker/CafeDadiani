9bc91756f827053d561a0ca33c54bcaed5bc7d53
Viktor Sivulskiy <svicvg@gmail.com>
2018-04-06 15:15:28 +0600

    The Consts LEADERS And PROMOTIONS Were Updated

--- src/app/shared/promotions.ts
+++ src/app/shared/promotions.ts
@@ -3,7 +3,7 @@ import { Promotion } from './promotion';
 export const PROMOTIONS: Promotion[] = [
     {
         id: 0,
-        name: 'Чашушули',
+        name: 'Супра',
         image: '/assets/images/buffet.png',
         label: 'New',
         price: '1 699',
