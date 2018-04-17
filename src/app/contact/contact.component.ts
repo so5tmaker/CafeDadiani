fe77e5d9c78cded3eacfb8a68897b70297a9c452
Viktor Sivulskiy <svicvg@gmail.com>
2018-04-17 14:59:19 +0600

    The Observable valueChanges Was Added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -15,6 +15,34 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   contactType = ContactType;
 
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required': 'Обязательно введите имя.',
+      'minlength': 'Имя должно быть длиной не менее 2-х символов.',
+      'maxlength': 'Имя не должно быть больше 25 символов.'
+    },
+    'lastname': {
+      'required': 'Обязательно введите фамилию.',
+      'minlength': 'Фамилия должна быть длиной не менее 2-х символов.',
+      'maxlength': 'Фамилия не должна быть больше 25 символов.'
+    },
+    'telnum': {
+      'required': 'Обязательно введите номер телефона.',
+      'pattern': 'Номер телефона должен быть цифровым.'
+    },
+    'email': {
+      'required': 'Обязательно введите Email.',
+      'email': 'Email неправильного формата.'
+    },
+  };
+
   constructor(private fb: FormBuilder) {
     this.createForm();
   }
@@ -22,16 +50,21 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
-  createForm() {
+  createForm(): void {
     this.feedbackForm = this.fb.group({
-      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
-      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
-      telnum: ['', [Validators.required, Validators.pattern] ],
-      email: ['', [Validators.required, Validators.email] ],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: ['', [Validators.required, Validators.pattern]],
+      email: ['', [Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
   }
 
   onSubmit() {
@@ -46,6 +79,22 @@ export class ContactComponent implements OnInit {
       contacttype: 'None',
       message: ''
     });
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
 }
