8a70062160b71c2b809c5669358dea32622f4fb0
Viktor Sivulskiy <svicvg@gmail.com>
2018-04-17 14:47:19 +0600

    The Form Model Was Updated

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -24,10 +24,10 @@ export class ContactComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: ['', Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      telnum: ['', [Validators.required, Validators.pattern] ],
+      email: ['', [Validators.required, Validators.email] ],
       agree: false,
       contacttype: 'None',
       message: ''
