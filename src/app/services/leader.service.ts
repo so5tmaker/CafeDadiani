b90a71a5dd244fc4533d72db528ef11ba4e997fc
Viktor Sivulskiy <svicvg@gmail.com>
2018-04-20 14:37:49 +0600

    The Leader Service Was Updated To Use HTTP

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -4,25 +4,34 @@ import { LEADERS } from '../shared/leaders';
 
 import { Observable } from 'rxjs/Observable';
 
+import { Http, Response } from '@angular/http';
+import { baseURL } from '../shared/baseurl';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
+
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class LeaderService {
 
-  constructor() { }
+  constructor(private http: Http,
+    private processHTTPMsgService: ProcessHTTPMsgService) { }
 
   getLeaders(): Observable<Leader[]> {
-    return Observable.of(LEADERS).delay(2000);
+    return this.http.get(baseURL + 'leaders')
+                    .map(res => { return this.processHTTPMsgService.extractData(res); });
   }
 
   getLeader(id: number): Observable<Leader> {
-    return Observable.of(LEADERS.filter((leader) => (leader.id === id))[0]).delay(2000);
+    return this.http.get(baseURL + 'leaders/' + id)
+                    .map(res => { return this.processHTTPMsgService.extractData(res); });
   }
 
   getFeaturedLeader(): Observable<Leader> {
-    return Observable.of(LEADERS.filter((leader) => leader.featured)[0]).delay(2000);
+    return this.http.get(baseURL + 'leaders/?featured=true')
+                    .map(res => { return this.processHTTPMsgService.extractData(res); });
   }
 
 }
