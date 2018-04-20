23265cf648e2a351164aff5edd8d1124d58895fd
Viktor Sivulskiy <svicvg@gmail.com>
2018-04-20 14:12:57 +0600

    The DishDetail Сщьзщтуте Was Updated To Use BaseURL

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Comment } from '../shared/comment';
@@ -45,7 +45,8 @@ export class DishdetailComponent implements OnInit {
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location,
-    private fb: FormBuilder) {
+    private fb: FormBuilder,
+    @Inject('BaseURL') private BaseURL) {
     this.createForm();
   }
 
