339c394a44c100faecf3aea4d9fa785f2dad7436
Viktor Sivulskiy <svicvg@gmail.com>
2018-04-20 16:22:25 +0600

    The About Component Was Updated To Use BaseURL

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
@@ -12,16 +12,11 @@ export class AboutComponent implements OnInit {
 
   leaders: Leader[];
 
-  selectedLeader: Leader;
-
-  constructor(private leaderService: LeaderService) { }
+  constructor(private leaderService: LeaderService,
+    @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
       this.leaderService.getLeaders().subscribe(leaders => this.leaders = leaders);
-  }
-
-  onSelect(leader: Leader) {
-    this.selectedLeader = leader;
   }
 
 }
