24f39f044edae48d3891d01d46ced535221ccd5c
Viktor Sivulskiy <svicvg@gmail.com>
2018-04-21 07:52:13 +0600

    The Process-httpmsg Service Was Updated To Handle Errors

--- src/app/services/process-httpmsg.service.ts
+++ src/app/services/process-httpmsg.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
 
+import 'rxjs/add/observable/throw';
+
 @Injectable()
 export class ProcessHTTPMsgService {
 
@@ -10,7 +12,23 @@ export class ProcessHTTPMsgService {
   public extractData(res: Response) {
     let body = res.json();
     console.log(body);
-    return body || { };
+    return body || {};
+  }
+
+  public handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 
 }
+
+
+
