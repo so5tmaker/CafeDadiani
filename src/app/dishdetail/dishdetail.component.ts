e182a0e7cf359e3e1433ef554081d8d3e792ca45
Viktor Sivulskiy <svicvg@gmail.com>
2018-04-27 15:25:17 +0600

    The Dish Preview Was Added

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -97,11 +97,9 @@ export class DishdetailComponent implements OnInit {
 
   onSubmit() {
     this.comment = this.commentForm.value;
-
-    this.dish.comments.push(this.comment);
-
-    let date = new Date();
-    this.comment.date = date.toISOString();
+    this.dishcopy.comments.push(this.comment);
+    this.dishcopy.save()
+      .subscribe(dish => { this.dish = dish; console.log(this.dish); });
 
     this.commentForm.reset({
       author: '',
